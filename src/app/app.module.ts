6d10e470a043086930513ad88b43c68e7d009fc1
Alexandre Guérin <alexandre.guerin17@gmail.com>
2019-06-23 12:44:38 +0200

    Implementation of interceptor to put jwt on each request and first impl of jwt management on authService WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +9,12 @@ import { WhyComponent } from './components/why/why.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, WhyComponent, RegisterFormComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root',
@@ -13,9 +15,33 @@ export class AuthService {
   };
 
   login(email: string, password: string) {
-    return this.http.post<any>('http://localhost:3000/auth', { email, password }, this.httpOptions);
-    // this is just the HTTP call,
-    // we still need to handle the reception of the token
-    // .shareReplay()
+    return this.http.post<any>('http://localhost:3000/auth', { email, password }, this.httpOptions).pipe(
+      map(jwt => {
+        this.setSession(jwt);
+        return jwt;
+      }),
+    );
   }
+
+  private setSession(authResult) {
+    localStorage.setItem('token', authResult);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  // public isLoggedIn() {
+  //   return moment().isBefore(this.getExpiration());
+  // }
+
+  // isLoggedOut() {
+  //   return !this.isLoggedIn();
+  // }
+
+  // getExpiration() {
+  //   const expiration = localStorage.getItem('expires_at');
+  //   const expiresAt = JSON.parse(expiration);
+  //   return moment(expiresAt);
+  // }
 }
